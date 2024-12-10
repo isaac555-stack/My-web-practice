607c6a664422ca82334a3bc392d9f31e0b5649f4
isaac555-stack <oparaisaacugochukwu@gmail.com>
2024-12-10 22:19:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,143 @@
-$("button").on("click", function () {
-  $(".reference").slideToggle();
+$("#hamburger").on("click", function () {
+  //-
+  $(".collapse").slideToggle(); //-
+}); //-
+//-
+$("#bell").on("click", function () {
+  //-
+  document.querySelector(".off-canvas").classList.toggle("open"); //-
+}); //-
+//-
+$(".removeCanvas").on("click", function () {
+  //-
+  $(".off-canvas").removeClass("open"); //-
+}); //-
+//-
+$(".container").on("click", function () {
+  //-
+  $(".off-canvas").removeClass("open"); //-
+  $(".collapse").slideUp(); //-
+}); //-
+// // Carousel functionality//+
+// const $track = $(".carousel-track");
+// const $slides = $(".carousel-item");
+// const totalSlides = $slides.length;
+// const $indicators = $(".carousel-indicator > span");//+
+
+// let currentSlideIndex = 0;//+
+// let intervalId;//+
+
+// function updateSlidePosition() {
+//   const newPosition = -currentSlideIndex * 100 + "%";
+//   $track.css("transform", `translateX(${newPosition})`);
+// }
+
+// function updateActiveSlide() {//+
+//   $indicators.removeClass("active");//+
+//   $($indicators[currentSlideIndex]).addClass("active");//+
+// }//+
+
+// function moveToSlide(index) {//+
+//   currentSlideIndex = (index + totalSlides) % totalSlides;//+
+
+// }//+
+
+// function nextSlide() {//+
+//   moveToSlide(currentSlideIndex + 1);//+
+// }//+
+// //+
+// function prevSlide() {//+
+//   moveToSlide(currentSlideIndex - 1);//+
+// }//+
+// //+
+// $(".btn-next").click(nextSlide);//+
+// //+
+// $(".btn-prev").click(prevSlide);//+
+// //+
+// // Allow clicking on indicators to jump to a slide//+
+// $indicators.each(function(index) {//+
+//   $(this).click(function() {
+//     moveToSlide(index);
+//     resetInterval();
+//   });
+// });
+
+// //+
+// function startInterval() {//+
+//   intervalId = setInterval(nextSlide, 3000);//+
+// }//+
+// //+
+// function resetInterval() {//+
+//   clearInterval(intervalId);//+
+//   startInterval();//+
+// }//+
+// //+
+// // Start automatic sliding//+
+// startInterval();//+
+// //+
+// // Pause on hover//+
+// $track.hover(//+
+//   function() { clearInterval(intervalId); },//+
+//   startInterval//+
+// );//+
+
+const $track = $(".carousel-track");
+const $slides = $(".carousel-item");
+const totalSlides = $slides.length;
+const $indicators = $(".carousel-indicator > span");
+
+let currentSlideIndex = 0;
+let intervalId;
+
+function updateSlidePosition() {
+  const newPosition = -currentSlideIndex * 100 + "%";
+  $track.css("transform", `translateX(${newPosition})`);
+}
+
+function updateActiveSlide() {
+  $indicators.removeClass("active");
+  $($indicators[currentSlideIndex]).addClass("active");
+}
+
+function moveToSlide(index) {
+  currentSlideIndex = (index + totalSlides) % totalSlides;
+  updateSlidePosition();
+  updateActiveSlide();
+}
+
+function nextSlide() {
+  moveToSlide(currentSlideIndex + 1);
+}
+
+function prevSlide() {
+  moveToSlide(currentSlideIndex - 1);
+}
+
+$(".btn-next").click(nextSlide);
+
+$(".btn-prev").click(prevSlide);
+
+// Allow clicking on indicators to jump to a slide
+$indicators.each(function (index) {
+  $(this).click(function () {
+    moveToSlide(index);
+    resetInterval();
+  });
 });
 
-$("button").hover(
-  function () {
-    $(this).addClass("pressed");
-  },
-  function () {
-    $(this).removeClass("pressed");
-  }
-);
+function startInterval() {
+  intervalId = setInterval(nextSlide, 3000);
+}
+
+function resetInterval() {
+  clearInterval(intervalId);
+  startInterval();
+}
+
+// Start automatic sliding
+startInterval();
+
+// Pause on hover
+$track.hover(function () {
+  clearInterval(intervalId);
+}, startInterval);
